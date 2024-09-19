92fc687119894b340a170806a823d8c0907503f0
sakibMERN <sakib.sr57@gmail.com>
2024-09-19 15:31:38 +0600

    add amount section functional colplete

--- js/home.js
+++ js/home.js
@@ -9,5 +9,26 @@ document.getElementById("btn-add-money").addEventListener("click", function(e){
     
     //get the pin
     const pinNumberInput = document.getElementById("input-pin-number").value;
-    console.log(pinNumberInput);
+    
+    //Step-3: Verify pin number
+    if(pinNumberInput === "1234" && addMoneyInput !== ""){
+        //Step-4: get the current balance
+        const balance = document.getElementById("account-balance").innerText;
+        // console.log(typeof balance); 
+
+        //Step-5: add addMoneyInput with balance
+        const addMoneyNumber = parseFloat(addMoneyInput);
+        const balanceNumber = parseFloat(balance);
+        
+        const newBalance = addMoneyNumber + balanceNumber;
+        console.log(newBalance);
+
+        //Step-6: update the balance in the UI/DOM
+
+        document.getElementById("account-balance").innerText = newBalance;
+        
+    }
+    else{
+        alert("Operation failed! Please try again.")
+    }
 })
